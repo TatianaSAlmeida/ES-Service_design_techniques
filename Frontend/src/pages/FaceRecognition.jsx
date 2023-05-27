61930ca83767b1419b17b53138316c3d4e80119b
EdgarTip <edgartip2003@gmail.com>
2023-05-27 15:06:14 +0100

    update

--- Frontend/src/pages/FaceRecognition.jsx
+++ Frontend/src/pages/FaceRecognition.jsx
@@ -6,23 +6,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './FaceRecognition.css';
 
+
 function Recognition() {
 
     const uploadedImage = React.useRef(null);
     const imageUploader = React.useRef(null);
-    
-    const handleImageUpload = e => {
+    let scriptText = React.useRef(null);
+
+    const [out, setOutput] = useState("Loading");
+
+
+
+    //Uploads a image
+    const uploadImage = e => {
+        //Gets file from event
         const [file] = e.target.files;
+
+        //Sees if file exists
         if (file) {
             const reader = new FileReader();
             const {current} = uploadedImage;
+
             current.file = file;
             reader.onload = (e) => {
                 current.src = e.target.result;
             }
+            
             reader.readAsDataURL(file);
         }
     };
+
+
+    // const runRekognitionScript = async(code) => {
+    //     const pyodide = window.pyodide;
+
+    //     return await pyodide.runPythonAsync(code);
+    // }
 
     return(
         <div className='body'>
@@ -31,18 +50,22 @@ function Recognition() {
                 <img src={image} className='img' alt="Image" />
             </div>  
 
-
-
+            <form className="grid" action="/result" method="POST">
+                <div className="img-container" onClick={() => imageUploader.current.click()}>
+                    <img className = 'img-profile' ref={uploadedImage}/>
+                </div>
             
-            <div className="img-container" onClick={() => imageUploader.current.click()}>
-                <img className = 'img-profile' ref={uploadedImage}/>
-            </div>
-
-            <input type="file" accept = "image/*" onChange={handleImageUpload} ref={imageUploader} className = "img-input"  alt="Submit" />
+                <input type="file" accept = "image/*" onChange={uploadImage} ref={imageUploader} className = "img-input"  alt="Submit" />
+                <center><input class="file_submit" type="Submit"/></center>
+            </form>
             
             <b>Click to upload Image</b>
             
-            <button>Submit for approval</button>
+            {/* <div className="items">
+                <button className='btn' onClick={() => runPythonFunction()}>Submit for approval</button>
+            </div> */}
+
+            <p>{out}</p>
             
         </div>
     )
