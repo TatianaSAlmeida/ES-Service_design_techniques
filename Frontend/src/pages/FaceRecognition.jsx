7bd0d4099e25028536c3886f5849086ae87347ac
EdgarTip <edgartip2003@gmail.com>
2023-05-27 01:14:08 +0100

    updated

--- Frontend/src/pages/FaceRecognition.jsx
+++ Frontend/src/pages/FaceRecognition.jsx
@@ -2,14 +2,28 @@ import image from '/src/assets/horizontal_img.png';
 import logo from '/src/assets/logo.png';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 import './FaceRecognition.css';
 
 function Recognition() {
-    const [image, setImage] = useState("");
-    var state = {
-        picture: false,
-        src: false
-    }
+
+    const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
+    
+    const handleImageUpload = e => {
+        const [file] = e.target.files;
+        if (file) {
+            const reader = new FileReader();
+            const {current} = uploadedImage;
+            current.file = file;
+            reader.onload = (e) => {
+                current.src = e.target.result;
+            }
+            reader.readAsDataURL(file);
+        }
+    };
+
     return(
         <div className='body'>
             <div className='images'>
@@ -19,11 +33,16 @@ function Recognition() {
 
 
 
-            <input type="file" width="40" height="40" alt="Submit"/>
-
-            <div className = 'img-container'>
-                <img className = 'img-profile'/>
+            
+            <div className="img-container" onClick={() => imageUploader.current.click()}>
+                <img className = 'img-profile' ref={uploadedImage}/>
             </div>
+
+            <input type="file" accept = "image/*" onChange={handleImageUpload} ref={imageUploader} className = "img-input"  alt="Submit" />
+            
+            <b>Click to upload Image</b>
+            
+            <button>Submit for approval</button>
             
         </div>
     )
