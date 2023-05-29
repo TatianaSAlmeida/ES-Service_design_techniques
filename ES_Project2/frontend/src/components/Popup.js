7fe752336a550e0547b06d1840698f74c3f4f530
TatianaSAlmeida <tatianasilvaalmeida24@gmail.com>
2023-05-29 22:29:30 +0100

    pop-up done

--- ES_Project2/frontend/src/components/Popup.js
+++ ES_Project2/frontend/src/components/Popup.js
@@ -42,33 +42,48 @@ function Popup(props){
                 </div>;
         
 
-    return(props.trigger) ? ( 
-        <div className="popup">
-            <div className="popup-inner">
-                <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
+        return props.trigger ? (
+            <div className="popup">
+              <div className="popup-inner">
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>
+                  close
+                </button>
                 {props.children}
                 <div>
-                <div className='row'> 
-                    <div className='column'> Prescription </div>
-                    <div className='column'> Paid </div>
-                    <div className='column'> Status </div>
-                    <div className='column'> Client Name </div>
-
+                  <div className="rowh">
+                    <div className="column">Prescription</div>
+                    <div className="column">Paid</div>
+                    <div className="column">Status</div>
+                    <div className="column">Client Name</div>
+                  </div>
+                  {purchases &&
+                    purchases.map((item, index) => (
+                      <div key={index} className="row">
+                        <div className="column">
+                          {item.fields.prescription && (
+                            <div className="drugList">
+                              {Object.entries(
+                                JSON.parse(
+                                  item.fields.prescription.replace(/'/g, '"')
+                                )
+                              ).map(([key, value]) => (
+                                <div key={key} className= 'row1'>
+                                  <div className="n">{value[0]}x</div>
+                                  <div className="n">{value[1].replace(" ", '')}</div>
+                                </div>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                        <div className="column">{item.fields.is_paid.toString()}</div>
+                        <div className="column">{item.fields.purchase_status}</div>
+                        <div className="column">{item.fields.client_name}</div>
+                      </div>
+                    ))}
                 </div>
-                {purchases && purchases.map((item, index) => (
-                    <div key={index} className= "row">
-                        <div className='column'> {item.fields.prescription} </div>
-                        <div className='column'>  {item.fields.is_paid.toString()} </div>
-                        <div className='column'> {item.fields.purchase_status} </div>
-                        <div className='column'> {item.fields.client_name} </div>
-                    </div>
-                ))}
-                </div>;
+              </div>
             </div>
-
-
-        </div>
-    ) : "";
+          ) : null;
 }
 
 export default Popup
